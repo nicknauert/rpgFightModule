7205ca6968eb16c96d1047385961a5e6e6899ad1
Nick Nauert <nicknauert@gmail.com>
2017-07-30 18:33:58 -0400

    added enemy turns and other improvements

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
 let eHealthBar = document.getElementById("enemyHlth");
-let punchBtn = document.getElementById("punch");
 let eHealthNum = document.getElementById("enemyHlthNum");
+let fHealthBar = document.getElementById("friendlyHlth");
+let fHealthNum = document.getElementById("myHlthNum");
 
-// these two functions are used to push the updated hp value to the text display
+
+
+
+// Characters ==========================
+let hero = {
+  name: "Gar'bo",
+  health: 100,
+  atk: 5, //dmg = dmg * (1 + (atk * 0.1)); i.e. dmg * 1.5
+  armor: 3, // dmg = dmg * (1 + (atk * 0.1)) / (1 + (armor * .05); i.e. dmg * 1.5 / 1.15
+}
+
+let baddie = {
+  name: "Squintz",
+  health: 80,
+  atk: 4,
+  armor: 2
+}
+
+
+
+
+
+// Health Updater Functions ============================
+
 function updEHealth(){
   eHealthNum.innerHTML = eHealthBar.value;
 }
+updEHealth();
 
 function updFHealth(){
   fHealthNum.innerHTML = fHealthBar.value;
 }
+updFHealth();
 
-// this is specific to the crit display, and re-useable for other things
-// the quirk is it can only apply to async functions....
+
+
+
+// crit display=============================
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// like this one
 async function critDisplay() {
   document.getElementById("crit").style.display = "block";
   console.log("crit");
@@ -25,33 +53,36 @@ async function critDisplay() {
   document.getElementById("crit").style.display = "none";
 }
 
-// actions
 
-function punch(){
-  // pulls a random # for determining dmg
+
+
+
+// actions ======================================
+
+async function punch(){
   let chance = Math.random();
   let dmg = 0;
 
-  eHealthBar = eHealthBar; //this may or may not be necessary anymore idk man
-  // setting damage levels for various chance outputs
   if (chance <= .1){
     dmg = Math.floor(Math.random()*6)+2;
   } else if (chance >.1 && chance <= .9){
     dmg = Math.floor(Math.random()*10)+4;
   } else if (chance >= .9){
     dmg = Math.floor(Math.random()*6)+12;
-    critDisplay(); //pops up the crit indicator if someone gets crat on
+    critDisplay();
   }
-  eHealthBar.value -= dmg; //updates enemy health with dmg out from above
-  console.log(dmg); //debugging
+  eHealthBar.value -= Math.round(dmg * (1+(hero.atk*.1)) / (1+(baddie.armor*.05)));
+  console.log(dmg);
   updEHealth();
+  await sleep(500);
+  enemyTurn();
+
 }
 
-function judo(){
+async function judo(){
   //higher risk of failure/crit. Low non-crit dmg.
   let chance = Math.random();
   let dmg = 0;
-  eHealthBar = eHealthBar;
   if (chance <= .15){
     dmg = Math.floor(Math.random()*7)+1;
   } else if (chance >.15 && chance <= .9){
@@ -60,21 +91,59 @@ function judo(){
     dmg = Math.floor(Math.random()*4)+18;
     critDisplay();
   }
-  eHealthBar.value -= dmg;
-  console.log(dmg);
+  eHealthBar.value -= Math.round(dmg * (1+(hero.atk*.1)) / (1+(baddie.armor*.05)));
+  console.log(Math.round(dmg * (1+(hero.atk*.1)) / (1+(baddie.armor*.05))));
   updEHealth();
+  await sleep(500);
+  enemyTurn();
 }
 
-// this was my test for the updated dmg model. does nothing
-function dmgRnd(){
-  let chance = Math.random();
-  let dmg = 0;
-  if (chance <= .1){
-    dmg = Math.floor(Math.random()*6)+2;
-  } else if (chance >.1 && chance <= .9){
-    dmg = Math.floor(Math.random()*10)+4;
-  } else if (chance >= .9){
-    dmg = Math.floor(Math.random()*6)+12;
 
+
+
+// Enemy function ==========================
+
+function enemyTurn() {
+  let moveChance = Math.random();
+  let dmg = 0;
+
+  function ePunch() {
+    chance = Math.random();
+
+    if (chance <= .1){
+      dmg = Math.floor(Math.random()*6)+2;
+    } else if (chance >.1 && chance <= .9){
+      dmg = Math.floor(Math.random()*10)+4;
+    } else if (chance >= .9){
+      dmg = Math.floor(Math.random()*6)+12;
+      critDisplay();
+    }
+    fHealthBar.value -= Math.round(dmg * (1+(baddie.atk*.1)) / (1+(hero.armor*.05)));
+    console.log(Math.round(dmg * (1+(baddie.atk*.1)) / (1+(hero.armor*.05))));
+    updFHealth();
   }
+
+  function eJudo(){
+    let chance = Math.random();
+
+    if (chance <= .15){
+      dmg = Math.floor(Math.random()*7)+1;
+    } else if (chance >.15 && chance <= .9){
+      dmg = Math.floor(Math.random()*8)+3;
+    } else if (chance >= .9){
+      dmg = Math.floor(Math.random()*4)+18;
+      critDisplay();
+    }
+    fHealthBar.value -= Math.round(dmg * (1+(baddie.atk*.1)) / (1+(hero.armor*.05)));
+    console.log(Math.round(dmg * (1+(baddie.atk*.1)) / (1+(hero.armor*.05))));
+    updFHealth();
+  }
+
+  if (moveChance>.5){
+    ePunch();
+  } else {
+    eJudo();
+  }
+
+
 }
